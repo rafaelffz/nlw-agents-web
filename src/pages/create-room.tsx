7a085925c6b706e81d9afef59a34234749995bb9
rafaelffz <devrafaelffz@gmail.com>
2025-07-12 04:21:16 -0300

    feat: finished project

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,40 +1,16 @@
-import { useQuery } from "@tanstack/react-query";
-import { Link } from "react-router-dom";
-
-type GetRoomsApiResponse = Array<{
-  id: string;
-  name: string;
-}>;
+import { CreateRoomForm } from "@/components/create-room-form";
+import { RoomList } from "@/components/room-list";
 
 export function CreateRoom() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["get-rooms"],
-    queryFn: async () => {
-      const response = await fetch("http://localhost:3333/rooms");
-      const result: GetRoomsApiResponse = await response.json();
-
-      return result;
-    },
-  });
-
   return (
-    <div>
-      <div>Create Room</div>
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <CreateRoomForm />
 
-      {isLoading && <div>Loading...</div>}
-      <div>
-        {data?.map((room) => {
-          return (
-            <Link key={room.id} to={`/room/${room.id}`}>
-              {room.name}
-            </Link>
-          );
-        })}
+          <RoomList />
+        </div>
       </div>
-
-      <Link className="underline" to={"/room"}>
-        Acessar Sala´"
-      </Link>
     </div>
   );
 }
